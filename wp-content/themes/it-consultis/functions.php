2959f57d13b4abd74d65fb7240d68bd6bce5450b
thongtr-dev <thongtr.dev@gmail.com>
2023-06-10 00:50:42 +0700

    Enable jQuery, enqueue owl carousel css and js

--- wp-content/themes/it-consultis/functions.php
+++ wp-content/themes/it-consultis/functions.php
@@ -143,15 +143,21 @@ add_action('widgets_init', 'it_consultis_widgets_init');
  */
 function it_consultis_scripts()
 {
-	wp_dequeue_script('jquery');
+	wp_enqueue_script("jquery");
 
-	wp_deregister_script('jquery');
+	wp_enqueue_style('it-consultis', get_stylesheet_uri());
 
-	wp_enqueue_style('it-consultis-style', get_stylesheet_uri());
+	wp_enqueue_style('it-consultis-index', get_template_directory_uri() . '/build/index.css', array(), _S_VERSION);
 
-	wp_enqueue_style('it-consultis-index-style', get_template_directory_uri() . '/build/index.css', array(), _S_VERSION);
+	wp_enqueue_style('it-consultis-owl-carousel', get_template_directory_uri() . '/css/owl.carousel.min.css', array(), _S_VERSION);
 
-	wp_enqueue_script('it-consultis-script', get_template_directory_uri() . '/build/index.js', array(), _S_VERSION, true);
+	wp_enqueue_script('it-consultis-owl-carousel', get_template_directory_uri() . '/js/owl.carousel.min.js', array(), _S_VERSION, true);
+
+	wp_enqueue_script('it-consultis-index', get_template_directory_uri() . '/build/index.js', array(), _S_VERSION, true);
+
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
+	}
 }
 add_action('wp_enqueue_scripts', 'it_consultis_scripts');
 
