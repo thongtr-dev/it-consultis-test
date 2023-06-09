9d40c0c866c2aa7d7f2cefeb874bb038323d24dd
thongtr-dev <thongtr.dev@gmail.com>
2023-06-10 00:48:04 +0700

    Add color vars in TailWind config

--- wp-content/themes/it-consultis/tailwind.config.js
+++ wp-content/themes/it-consultis/tailwind.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   content: ["./*.php", "./**/*.php", "./src/modules/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        "itc-light-gray": "var(--itc-light-gray)",
+        "itc-dark-red": "var(--itc-dark-red)",
+        "itc-secondary-black": "var(--itc-secondary-black)",
+        "itc-third-black": "var(--itc-third-black)"
+      },
+    },
+  },
 };
